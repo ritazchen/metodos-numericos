a61fae843bf546a7da55aa34800a6441194fedbb
Rita Chen <ritazhen1@hotmail.com>
2021-04-16 21:00:35 -0300

    Regra dos Trapézios Simples (Método Newton-Cotes)

--- TrapeziosSimples.c
+++ TrapeziosSimples.c
@@ -1,26 +1,53 @@
+/*
+Métodos Numéricos - Integração Numérica
+Método Newton-Cotes: Regra dos Trapézios Simples (apenas 1 trapézio)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 #define F(x) pow(x, 2)
 
+//#define RESULTADO_EXATO 1/3.0
+
 double regraDosTrapeziosSimples(double x0, double x1, double h);
+double Abs(double x);
+double erroAbs(double resultadoExato, double integralEncontrada);
 
 int main(int argc, char* argv[])
 {
-    double x0, x1;
+    double x0, x1, h, integral;
     
-    printf("Informe o x0, x1: ");
+    printf("Informe o x0 e x1: ");
     scanf("%lf %lf", &x0, &x1);
+    h = x1 - x0;
 
-    printf("Resultado da integral = %lf\n", regraDosTrapeziosSimples(x0, x1, (x1-x0)));
-
+    printf("\nParametros: x0 = %lf\txn = %lf\t\th = %.16lf\n\n", x0, x1, h);
+    
+    integral = regraDosTrapeziosSimples(x0, x1, h);
+    printf("\n\t-> Resultado da integral = %.16lf\n", integral);
+    
+    //printf("\t-> Erro absoluto (EA) = %.16lf\n", erroAbs(RESULTADO_EXATO, integral));
+    
     return 0;
 }
 
 double regraDosTrapeziosSimples(double x0, double x1, double h) 
 {   
-    printf("Parametros:\nx0 = %lf\t\tx1 = %lf\t\th = %lf\n", x0, x1, h);
-    printf("F(x0) = %lf\tF(x1) = %lf\th/2 = %lf\n", F(x0), F(x1), (h/2));
+    printf("F(x0) = %.16lf\tF(x1) = %.16lf\th/2 = %lf\n", F(x0), F(x1), (h/2));
     return ( (F(x0) + F(x1)) * (h/2) );
+}
+
+double erroAbs(double resultadoExato, double integralEncontrada) {
+    return Abs(resultadoExato - integralEncontrada);
+}
+
+double Abs(double x) {
+    if(x > 0) {
+        return x;
+    }
+    else {
+        return x * (-1);
+    }
 }
---
+++ UmTercoDeSimpson.c
@@ -0,0 +1,58 @@
+/*
+Métodos Numéricos - Integração Numérica
+Método Newton-Cotes: Regra 1/3 de Simpson
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//#define F(x) exp(x) //questão 1 da lista
+#define F(x) (1/x)*exp(x/2) //questão 2 da lista
+//#define F(x) (1/(1+x)) //questão 3 da lista
+
+double regraUmTercoSimpson(double x0, double xn, double h);
+
+int main(int argc, char* argv[])
+{
+    double x0, xn, numIntervalos, h;
+    
+    printf("Informe o x0, xn e o número de intervalos: ");
+    scanf("%lf %lf %lf", &x0, &xn, &numIntervalos);
+    h = ((xn-x0)/numIntervalos);
+
+    printf("\nParametros: x0 = %lf\txn = %lf\t\tnúmero de intervalos = %.0lf\t\th = %.16lf\n\n", x0, xn, numIntervalos, h);
+
+    printf("\n\t\t\t\t\t-> Resultado da integral = %.16lf\n", regraUmTercoSimpson(x0, xn, h));
+
+    return 0;
+}
+
+double regraUmTercoSimpson(double x0, double xn, double h) 
+{
+    double integral = 0.0;
+    double i;
+    int iteracao = 0, peso;
+    
+    printf("iterações\t\ti\t\t\t       F(xi)\t\t\t    ci (peso)\t\t    ci * f(xi)\n");
+    printf("    %d\t\t    %lf\t\t\t%.16lf\t\t\t%d\t\t%.16lf\n", iteracao++, x0, F(x0), 1, F(x0));
+
+    for(i = (x0 + h); i < xn; i += h) {
+        if(iteracao % 2 == 0) {
+            integral += (2.0*F(i));
+            peso = 2;
+        }
+        else {
+            integral += (4.0*F(i));
+            peso = 4;
+        }
+        printf("    %d\t\t    %lf\t\t\t%.16lf\t\t\t%d\t\t%.16lf\t\t\n", iteracao++, i, F(i), peso, (2*F(i)));
+    }
+
+    printf("    %d\t\t    %lf\t\t\t%.16lf\t\t\t%d\t\t%.16lf\n", iteracao, xn, F(xn), 1, F(xn));
+
+    integral += F(x0) + F(xn);
+    integral = integral * (h/3);
+
+    return integral;
+}
