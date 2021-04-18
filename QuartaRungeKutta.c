6f31ae1204cfca2851a92a6d674b4b2b1d342d1a
Rita Chen <ritazhen1@hotmail.com>
2021-04-18 15:36:01 -0300

    Runge-Kutta de 4a ordem (Soluções Numéricas de EDOs)

--- QuartaRungeKutta.c
+++ QuartaRungeKutta.c
@@ -7,10 +7,11 @@ Método de Runge-Kutta de 4a ordem
 #include <stdlib.h>
 #include <math.h>
 
-#define F(x,y) -2*y //y' = f(x,y)
+//#define F(x,y) -2*y //y' = f(x,y)
+#define F(x,y) ((-y) + x + 2)
 
-#define EXATO(x,y) 1/exp(2*x) //função exata para o exemplo do slide (nem sempre vamos ter essa formula)
-//#define EXATO(x,y) (exp(-x) + x + 1) //funcao exata da lista
+//#define EXATO(x,y) 1/exp(2*x) //função exata para o exemplo do slide (nem sempre vamos ter essa formula)
+#define EXATO(x,y) ((exp(-x)) + x + 1) //funcao exata da lista
 
 double quartaOrdem(double x0, double xn, double y0, double h);
 double Abs(double x);
@@ -22,36 +23,44 @@ double K4(double x, double y, double h);
 
 int main(int argc, char* argv[])
 {
-    //double x0 = 0, xn = 1, y0 = 1, h = 0.2, yn;
-    
+    double x0 = 0, xn = 1.5, y0 = 2, h = 0.1, yn;
+    /*
     printf("Informe o x0, y0, xn e h: ");
     double x0, y0, xn, h, yn;
     scanf("%lf %lf %lf %lf", &x0, &y0, &xn, &h);
-
+    */
     printf("\nParametros: x0 = %lf\ty0 = %lf\t\txn = %lf\t\th = %lf\n\n", x0, y0, xn, h);
 
     yn = quartaOrdem(x0,xn,y0,h);
     printf("\n\t\t\t\t\t-> Para x=%.2lf, a solução y eh %.16lf\n", xn, yn);
     
-    printf("\t\t\t\t\t-> Erro absoluto (EA) = %.16lf\n", erroAbs(yn, EXATO(xn,yn)));
+    //printf("\t\t\t\t\t-> Erro absoluto (EA) = %.16lf\n", erroAbs(yn, EXATO(xn,yn)));
 
     return 0;
 }
 
 double quartaOrdem(double x0, double xn, double y0, double h)
 {
+    //FILE* f = fopen("dados_quarta.txt", "w");
 
     double i, y = y0, x = x0;
     int iteracao = 0;
 
-    printf("iterações\t\t   xi\t\t\t   yi\t\t\tK1\t\t\t\tK2\t\t\t\tK3\t\t\tK4\t\t\t    sol. exata\t\t\t\t   EA\n");
-    printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x, y, h), K4(x, y, h), EXATO(x,y), 0.0);
+    //printf("iterações\t  xi\t\t   yi\t\t   K1\t\t   K2\t\t   K3\t\t   K4\t\t   sol. exata\t\t\t      EA\n");
+    //printf("    %d\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x, y, h), K4(x, y, h), EXATO(x,y), 0.0);
+
+    printf("iterações\t  xi\t\t   yi\t\t   K1\t\t   K2\t\t   K3\t\t   K4\n");
+    printf("    %d\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x, y, h), K4(x, y, h));
+    //fprintf(f, "%lf,%lf;", x, y);
 
     for(x = x0 + h; iteracao <= ((xn-x0)/h); x = x + h) {
         y = y + ((h/6)*(K1(x,y,h) + (2*K2(x,y,h)) + (2*K3(x,y,h)) + K4(x,y,h)));
-        printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x,y,h), K4(x, y, h), EXATO(x,y), erroAbs(y, EXATO(x,y)));
+        //fprintf(f, "%lf,%lf;", x, y);
+        //printf("    %d\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x,y,h), K4(x, y, h), EXATO(x,y), erroAbs(y, EXATO(x,y)));
+        printf("    %d\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x,y,h), K4(x, y, h));
     }
 
+    //fclose(f);
     return y;
 }
 
