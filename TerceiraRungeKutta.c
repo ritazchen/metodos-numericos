f3ec2c64038bceabf0a5bdfe24147b7ff8f2feb6
Rita Chen <ritazhen1@hotmail.com>
2021-04-18 15:34:57 -0300

    Runge-Kutta de 3a ordem (Soluções Numéricas de EDOs)

--- TerceiraRungeKutta.c
+++ TerceiraRungeKutta.c
@@ -7,10 +7,14 @@ Método de Runge-Kutta de 3a ordem
 #include <stdlib.h>
 #include <math.h>
 
-#define F(x,y) -2*y //y' = f(x,y)
+//#define F(x,y) -2*y //y' = f(x,y)
+//#define F(x,y) ((-y) + x + 2)
+//#define F(x,y) ((pow(x,2)*y - 2)/x) //questão 3
+#define F(x,y) -2*x*(pow(y,2)) //questão 5
 
-#define EXATO(x,y) 1/exp(2*x) //função exata para o exemplo do slide (nem sempre vamos ter essa formula)
-//#define EXATO(x,y) (exp(-x) + x + 1) //funcao exata da lista
+//#define EXATO(x,y) 1/exp(2*x) //função exata para o exemplo do slide (nem sempre vamos ter essa formula)
+//#define EXATO(x,y) ((exp(-x)) + x + 1) //funcao exata da lista
+#define EXATO(x,y) 1/((pow(x,2))+2) //funcao exata da questão 5
 
 double terceiraOrdem(double x0, double xn, double y0, double h);
 double Abs(double x);
@@ -21,12 +25,12 @@ double K3(double x, double y, double h);
 
 int main(int argc, char* argv[])
 {
-    //double x0 = 0, xn = 1, y0 = 1, h = 0.2, yn;
-    
+    double x0 = 0, xn = 1, y0 = 0.5, h = pow(10,-3), yn;
+    /*
     printf("Informe o x0, y0, xn e h: ");
     double x0, y0, xn, h, yn;
     scanf("%lf %lf %lf %lf", &x0, &y0, &xn, &h);
-
+    */
     printf("\nParametros: x0 = %lf\ty0 = %lf\t\txn = %lf\t\th = %lf\n\n", x0, y0, xn, h);
 
     yn = terceiraOrdem(x0,xn,y0,h);
@@ -39,18 +43,26 @@ int main(int argc, char* argv[])
 
 double terceiraOrdem(double x0, double xn, double y0, double h)
 {
+    //FILE* f = fopen("dados_terceira.txt", "w");
 
     double i, y = y0, x = x0;
     int iteracao = 0;
-
-    printf("iterações\t\t   xi\t\t\t   yi\t\t\tK1\t\t\t   K2\t\t\t\tK3\t\t\t    sol. exata\t\t\t\t   EA\n");
-    printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x, y, h), EXATO(x,y), 0.0);
+    
+    //printf("iterações\t  xi\t\t   yi\t\t   K1\t\t   K2\t\t   K3\t\t   sol. exata\t\t\t      EA\n");
+    //printf("    %d\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x, y, h), EXATO(x,y), 0.0);
+    
+    printf("iterações\t  xi\t\t   yi\t\t   K1\t\t   K2\t\t   K3\n");
+    printf("    %d\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x, y, h));
+    //fprintf(f, "%lf,%lf;", x, y);
 
     for(x = x0 + h; iteracao <= ((xn-x0)/h); x = x + h) {
         y = y + ((2/9.0)*K1(x, y, h)) + ((1/3.0)*K2(x, y, h)) + ((4/9.0)*K3(x, y, h));
-        printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x,y,h), EXATO(x,y), erroAbs(y, EXATO(x,y)));
+        //fprintf(f, "%lf,%lf;", x, y);
+        //printf("    %d\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x,y,h), EXATO(x,y), erroAbs(y, EXATO(x,y)));
+        printf("    %d\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\t\t%.5lf\n", iteracao++, x, y, K1(x,y,h), K2(x,y,h), K3(x,y,h));
     }
 
+    //fclose(f);
     return y;
 }
 
