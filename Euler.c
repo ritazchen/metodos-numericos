8f9164dfacabe878f52874de835a336765bf8dfa
Rita Chen <ritazhen1@hotmail.com>
2021-04-19 20:36:08 -0300

    Método de Euler corrigido

--- Euler.c
+++ Euler.c
@@ -11,13 +11,13 @@ Método de Euler
 //#define F(x,y) ((-y) + x + 2) //questão 2
 //#define F(x,y) ((pow(x,2)*y - 2)/x) //questão 3
 //#define F(x,y) ((6*(pow(x,2))) - (1/(pow(x,2))) + 3) //questão 4
-//#define F(x,y) -2*x*(pow(y,2)) //questão 5
-#define F(x,y) x * (sqrt((pow(x,2)) + 5)) //questão 6
+#define F(x,y) -2*x*(pow(y,2)) //questão 5
+//#define F(x,y) x * (sqrt((pow(x,2)) + 5)) //questão 6
 
 //#define EXATO(x,y) 1/exp(2*x) //função exata para o exemplo do slide (nem sempre vamos ter essa formula)
-//#define EXATO(x,y) ((exp(-x)) + x + 1) //funcao exata da questão 2
-//#define EXATO(x,y) 1/((pow(x,2))+2) //funcao exata da questão 5
-#define EXATO(x,y) (1/3.0) * (pow((sqrt((pow(x,2)) + 5)),3)) - 1 //funcao exata da questão 6
+//#define EXATO(x,y) ((exp(x*(-1))) + x + 1) //funcao exata da questão 2
+#define EXATO(x,y) 1/((pow(x,2))+2) //funcao exata da questão 5
+//#define EXATO(x,y) (1/3.0) * (pow((sqrt((pow(x,2)) + 5)),3)) - 1 //funcao exata da questão 6
 
 double metodoDeEuler(double x0, double xn, double y0, double h);
 double Abs(double x);
@@ -25,7 +25,13 @@ double erroAbs(double resultadoExato, double integralEncontrada);
 
 int main(int argc, char* argv[])
 {
-    double x0 = 2, xn = 3, y0 = 8, h = .01, yn;
+    //double x0 = 0, xn = 1, y0 = 1, h = 0.2, yn; //exemplo do slide
+    //double x0 = 0, xn = 1.50, y0 = 2, h = 0.1, yn; //questão 2
+    //double x0 = 1, xn = 2, y0 = 1, h = 0.01, yn; //questão 3
+    //double x0 = 10, xn = 16, y0 = 0, h = 0.00001, yn; //questão 4
+    double x0 = 0, xn = 1, y0 = 0.5, h = 0.001, yn; //questão 5
+    //double x0 = 2, xn = 3, y0 = 8, h = 0.1, yn; //questão 6
+
     /*
     printf("Informe o x0, y0, xn e h: ");
     double x0, y0, xn, h, yn;
@@ -45,21 +51,25 @@ double metodoDeEuler(double x0, double xn, double y0, double h)
 {
     //FILE* f = fopen("dados_euler.txt", "w");
 
-    double i, y = y0, x = x0;
+    double i, y = y0, x = x0, X0 = x0;
     int iteracao = 0;
 
-    //printf("iterações\t\txi\t\t   yi\t\ty' = f(x,y)\t\t   sol. exata\t\t\t\tEA\n");
-    //printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x0, y0, F(x0,y0), EXATO(x0,y0), 0.0);
+    printf("iterações\t\txi\t\t   yi\t\ty' = f(x,y)\t\t   sol. exata\t\t\t\tEA\n");
+    printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x0, y0, F(x0,y0), EXATO(x0,y0), 0.0);
     
-    printf("iterações\t\txi\t\t   yi\t\ty' = f(x,y)\n");
-    printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\n", iteracao++, x, y, F(x,y));
+    //printf("iterações\t\txi\t\t   yi\t\ty' = f(x,y)\n");
+    //printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\n", iteracao++, x, y, F(x,y));
     //fprintf(f, "%lf,%lf;", x, y);
 
-    for(x = x0 + h; iteracao <= ((xn-x0)/h); x = x + h) {
-        y = y + h*F(x,y);
+    while(iteracao <= ((xn-X0)/h)) {
+        x0 = x;
+        y0 = y;
+        x = x0 + h;
+        y = y0 + (F(x0,y0) * h);
         //fprintf(f, "%lf,%lf;", x, y);
-        //printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, F(x,y), EXATO(x,y), erroAbs(y, EXATO(x,y)));
-        printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\n", iteracao++, x, y, F(x,y));
+        printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\t\t%.16lf\t\t%.16lf\n", iteracao++, x, y, F(x,y), EXATO(x,y), erroAbs(y, EXATO(x,y)));
+        
+        //printf("    %d\t\t     %.5lf\t\t%.5lf\t\t%.5lf\n", iteracao++, x, y, F(x,y));
     }
 
     //fclose(f);
@@ -71,7 +81,7 @@ double erroAbs(double resultadoExato, double integralEncontrada) {
 }
 
 double Abs(double x) {
-    if(x > 0) {
+    if(x >= 0) {
         return x;
     }
     else {
