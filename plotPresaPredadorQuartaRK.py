53c5e03b347bf4b47de4b6e9d82307a6f98792df
ritazchen <40496503+ritazchen@users.noreply.github.com>
2021-04-25 13:03:05 -0300

    Gráfico com o método RK4

---
+++ plotPresaPredadorQuartaRK.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import math
+
+X = []
+Y_presa = []
+Y_predador = []
+
+arquivo_presa = open('dados_presa_4RK.txt', 'r')
+dados_presa = arquivo_presa.read()
+arquivo_presa.close()
+
+arquivo_predador = open('dados_predador_4RK.txt', 'r')
+dados_predador = arquivo_predador.read()
+arquivo_predador.close()
+
+
+coordenadas = dados_presa.split(';')
+
+for coordenada in coordenadas:
+    if coordenada != '':
+        x, y = map(float, coordenada.split(','))
+        X.append(x)
+        Y_presa.append(y)
+
+X = []
+
+coordenadas = dados_predador.split(';')
+
+for coordenada in coordenadas:
+    if coordenada != '':
+        x, y = map(float, coordenada.split(','))
+        X.append(x)
+        Y_predador.append(y)
+
+plt.title('Modelo Presa-Predador RK4')
+plt.ylabel('População')
+plt.xlabel('Tempo')
+
+plt.plot(X, Y_presa, c='red', label = 'Presa')
+plt.plot(X, Y_predador, c='blue', label = 'Predador')
+#plt.plot(X, Y_predador, c='blue', marker='o', ls='--', label = 'Predador')
+
+plt.legend()
+plt.show()
